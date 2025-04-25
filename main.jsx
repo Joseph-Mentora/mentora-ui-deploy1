0b34d07368d7453471fa7eaa239a77e5c0eab54d
Joseph-Mentora <jmiller@internationalsommelier.com>
2025-04-25 00:58:36 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MentoraChatbot from './MentoraChatbot'
-import './style.css'
 
-ReactDOM.createRoot(document.getElementById('app')).render(
-  <React.StrictMode>
-    <MentoraChatbot />
-  </React.StrictMode>
-)
